390fbc719c4bf8c089b545415941b6d9b761a48d
TumanGolim <130469755+TumanGolim@users.noreply.github.com>
2023-09-25 22:15:02 +0300

    task1

---
+++ js/01-gallery.js
@@ -0,0 +1,46 @@
+import { galleryItems } from "./gallery-items.js";
+
+const gallery = document.querySelector(".gallery");
+
+function createGalleryItem(item) {
+  const galleryItem = document.createElement("li");
+  galleryItem.classList.add("gallery__item");
+
+  const link = document.createElement("a");
+  link.classList.add("gallery__link");
+  link.href = item.original;
+
+  const image = document.createElement("img");
+  image.classList.add("gallery__image");
+  image.src = item.preview;
+  image.alt = item.description;
+
+  image.setAttribute("data-original", item.original);
+
+  link.appendChild(image);
+  galleryItem.appendChild(link);
+
+  return galleryItem;
+}
+
+function openLightbox(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const imageSrc = event.target.getAttribute("data-original"); 
+  const alt = event.target.getAttribute("alt");
+
+  const lightbox = basicLightbox.create(
+    `<img src="${imageSrc}" alt="${alt}" width="800" height="600">`
+  );
+
+  lightbox.show();
+}
+
+gallery.addEventListener("click", openLightbox);
+
+const galleryItemsMarkup = galleryItems.map(createGalleryItem);
+gallery.append(...galleryItemsMarkup);
