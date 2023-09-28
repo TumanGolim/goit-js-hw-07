5c90f603a848067332d4462881f5cc355aec8c04
TumanGolim <130469755+TumanGolim@users.noreply.github.com>
2023-09-28 21:33:22 +0300

    task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+function createGalleryItem(item) {
+  const galleryItem = document.createElement("li");
+  galleryItem.classList.add("gallery__item");
+
+  const link = document.createElement("a");
+  link.classList.add("gallery__link");
+  link.href = item.original;
+
+  const image = document.createElement("img");
+  image.classList.add("gallery__image");
+  image.src = item.preview;
+  image.alt = item.description;
+
+  image.setAttribute("data-original", item.original);
+
+  link.appendChild(image);
+  galleryItem.appendChild(link);
+
+  return galleryItem;
+}
+
+function initializeLightbox() {
+  const galleryItemsMarkup = galleryItems.map(createGalleryItem);
+  gallery.append(...galleryItemsMarkup);
+
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 500,
+  });
+}
+
+initializeLightbox();
